0ed1bb0b8c777e0b799aff745064e97d56aa423d
Ruslan <ibrmov.rusl@mail.ru>
2025-11-19 00:56:55 +0500

    feat(simulation): add assembly logic, add python motorsim and extra submodules; fix numbering mode checking

--- simulation/src/main.cpp
+++ simulation/src/main.cpp
@@ -8,6 +8,8 @@
 #include "gpio_test.h"
 #include "iostream"
 #include "pin.h"
+#include "assembly.h"
+
 
 namespace py = pybind11;
 //TO DO: комментарии к функциям
@@ -16,24 +18,36 @@ namespace py = pybind11;
 enum class PinNumberingMode {
 	NONE, // по умолчанию
 	BOARD,
-	BCM,
 	SUNXI
 };
 
 
-static std::unique_ptr<OrangePi3LTS> g_board;
+// Умный указатель на Assembly — владеет объектом и управляет временем жизни
+static std::unique_ptr<Assembly> g_assembly;
+
+// Обычный указатель на объект платы внутри Assembly.
+// Объект управляется g_assembly, поэтому g_board не владеет памятью
+static const OrangePi3LTS* g_board;
+
+// Умный указатель на KsBinder — владеет объектом
 static std::unique_ptr<KsBinder> g_binder;
+
 static PinNumberingMode g_numberingMode = PinNumberingMode::NONE;
 
 
-void init_board() {
+void initBoard() {
 	consoleUtils::printMessage(L"KOPIS (Kompas-3D Orange Pi Simulator)\n\n");
 
 	consoleUtils::printMessage(L"Setting up board...\n");
 
 	auto startTimeSetupBoard = std::chrono::high_resolution_clock::now();
-	g_board = std::make_unique<OrangePi3LTS>();
-	g_binder = std::make_unique<KsBinder>(*g_board);
+	g_assembly = std::make_unique<Assembly>();
+
+	// Взять адрес объекта платы внутри Assembly и присвоить указателю
+	// g_board не будет владеть объектом, просто указывает на существующий объект
+	g_board = &g_assembly->getBoard();
+
+	g_binder = std::make_unique<KsBinder>(*g_assembly);
 	auto endTimeSetupBoard = std::chrono::high_resolution_clock::now();
 
 	auto durationInMillisecondsSetupBoard = duration_cast<std::chrono::milliseconds>(endTimeSetupBoard - startTimeSetupBoard);
@@ -41,13 +55,36 @@ void init_board() {
 		std::to_wstring(durationInMillisecondsSetupBoard.count()) + L" ms\n");
 }
 
+
+// Проверка требуемого режима нумерации пинов
+void checkRequiredNumberingMode(PinNumberingMode reqNumberingMode) {
+	if (g_numberingMode == reqNumberingMode) {
+		return;
+	}
+
+	if (g_numberingMode == PinNumberingMode::NONE) {
+		throw std::runtime_error("GPIO numbering mode is not set. Use ""GPIO.setmode()"" first");
+	}
+
+	if ((g_numberingMode != reqNumberingMode) && (reqNumberingMode == PinNumberingMode::BOARD)) {
+		throw (std::runtime_error("GPIO numbering mode by BOARD is not set. Use ""GPIO.setmode(GPIO.BOARD)"" first"));
+	}
+
+	if ((g_numberingMode != reqNumberingMode) && (reqNumberingMode == PinNumberingMode::SUNXI)) {
+		throw (std::runtime_error("GPIO numbering mode by SUNXI is not set. Use ""GPIO.setmode(GPIO.SUNXI)"" first"));
+	}
+
+	throw (std::runtime_error("Unknown GPIO numbering mode"));
+}
+
+
 // Capsule класс для управления временем жизни
 struct BoardCapsule {
 	std::chrono::high_resolution_clock::time_point startTimeRunGpioProg;
 
 	BoardCapsule() {
 		// инициализация объектов при загрузке модуля
-		init_board();
+		initBoard();
 
 		consoleUtils::printMessage(L"\nRunning GPIO program...\n");
 		startTimeRunGpioProg = std::chrono::high_resolution_clock::now();
@@ -62,9 +99,11 @@ struct BoardCapsule {
 
 		// очистка указателей на объекты при выгрузке модуля
 		g_binder.reset();
-		g_board.reset();
+		g_assembly.reset();
 	}
 };
+
+
 
 PYBIND11_MODULE(kopis, m) {
 	m.doc() = "KOPIS (Kompas-3D Orange Pi Simulator) Python module";
@@ -77,7 +116,7 @@ PYBIND11_MODULE(kopis, m) {
 
 	py::enum_<PinNumberingMode>(m, "PinNumberingMode")
 		.value("BOARD", PinNumberingMode::BOARD)
-		.value("BCM", PinNumberingMode::BCM)
+		.value("BCM", PinNumberingMode::BOARD) // BCM - аналог BOARD
 		.value("SUNXI", PinNumberingMode::SUNXI)
 		.export_values();
 
@@ -110,40 +149,48 @@ PYBIND11_MODULE(kopis, m) {
 
 
 	m.def("setup", [](const int pinNumber, const GpioMode mode) {
-		if (g_numberingMode == PinNumberingMode::NONE) {
-			throw (std::runtime_error("GPIO numbering mode is not set"));
-		}
+		checkRequiredNumberingMode(PinNumberingMode::BOARD);
 		GpioPin& gpioPin = static_cast<GpioPin&>(g_board->getPinByBoardNumber(pinNumber));
 		gpioPin.setMode(mode);
 	}, "set selected pin (by number) to INPUT or OUTPUT mode");
 	
 
 	m.def("setup", [](const std::string& socName, const GpioMode mode) {
-		if (g_numberingMode == PinNumberingMode::NONE) {
-			throw std::runtime_error("GPIO numbering mode is not set");
-		}
+		checkRequiredNumberingMode(PinNumberingMode::SUNXI);
 		GpioPin& gpioPin = static_cast<GpioPin&>(g_board->getPinBySocName(socName));
 		gpioPin.setMode(mode);
 	}, "set selected pin (by socket name) to INPUT or OUTPUT mode");
 
 
 	m.def("output", [](const int pinNumber, const GpioState state) {
-		if (g_numberingMode == PinNumberingMode::NONE) {
-			throw std::runtime_error("GPIO numbering mode is not set");
-		}
+		checkRequiredNumberingMode(PinNumberingMode::BOARD);
 		GpioPin& gpioPin = static_cast<GpioPin&>(g_board->getPinByBoardNumber(pinNumber));
 		gpioPin.setState(state);
 	}, "set selected pin's value (by number) as HIGH or LOW");
+
+
+	m.def("output", [](const int pinNumber, int value) {
+		checkRequiredNumberingMode(PinNumberingMode::BOARD);
+		GpioPin& gpioPin = static_cast<GpioPin&>(g_board->getPinByBoardNumber(pinNumber));
+		GpioState state = (value == 0) ? GpioState::LOW : GpioState::HIGH;
+		gpioPin.setState(state);
+	}, "set selected pin's value (by number) as 1 or 0");
 	
 
 	m.def("output", [](const std::string& socName, const GpioState state) {
-		if (g_numberingMode == PinNumberingMode::NONE) {
-			throw std::runtime_error("GPIO numbering mode is not set");
-		}
+		checkRequiredNumberingMode(PinNumberingMode::SUNXI);
 		GpioPin& gpioPin = static_cast<GpioPin&>(g_board->getPinBySocName(socName));
 		gpioPin.setState(state);
 	}, "set selected pin's value (by socket name) as HIGH or LOW");
-		
+	
+
+	m.def("output", [](const std::string& socName, int value) {
+		checkRequiredNumberingMode(PinNumberingMode::SUNXI);
+		GpioPin& gpioPin = static_cast<GpioPin&>(g_board->getPinBySocName(socName));
+		GpioState state = (value == 0) ? GpioState::LOW : GpioState::HIGH;
+		gpioPin.setState(state);
+	}, "set selected pin's value (by socket name) as 1 or 0");
+
 
 	m.def("setwarnings", [](bool isWarningsSet) {
 		if (isWarningsSet) {
@@ -166,19 +213,48 @@ PYBIND11_MODULE(kopis, m) {
 
 
 	m.def("cleanup", [](const int pinNumber) {
-		if (g_numberingMode == PinNumberingMode::NONE) {
-			throw (std::runtime_error("GPIO numbering mode is not set"));
-		}
+		checkRequiredNumberingMode(PinNumberingMode::BOARD);
 		GpioPin& gpioPin = static_cast<GpioPin&>(g_board->getPinByBoardNumber(pinNumber));
 		gpioPin.setMode(GpioMode::INPUT);
 	}, "set selected pin (by number) to the default mode");
 
 
 	m.def("cleanup", [](const std::string& socName) {
-		if (g_numberingMode == PinNumberingMode::NONE) {
-			throw std::runtime_error("GPIO numbering mode is not set");
-		}
+		checkRequiredNumberingMode(PinNumberingMode::SUNXI);
 		GpioPin& pin = static_cast<GpioPin&>(g_board->getPinBySocName(socName));
 		pin.setMode(GpioMode::INPUT);
 	}, "set selected pin (by socket name) to the default mode");
+
+
+
+	// Подмодуль для управления моторами в симуляторе
+	py::module_ kopis_motorsim = m.def_submodule("kopis_motorsim", "Motor control submodule for simulator");
+
+	kopis_motorsim.def("move_degrees", [](const std::string& motorName, const double angle, const int speed) {
+		// Принимаем string из-за ограничений Python и конвертируем в wstring
+		Motor& motor = g_assembly->getMotor(strUtils::strToWStr(motorName));
+		motor.rotateShaftAngle(angle, speed);
+	}, "rotate motor by name, angle and speed");
+
+	kopis_motorsim.def("reset_angle", [](const std::string& motorName) {
+		Motor& motor = g_assembly->getMotor(strUtils::strToWStr(motorName));
+		motor.resetShaftAngle();
+		}, "reset motor angle by name");
+
+
+
+	// Подмодуль для дополнительных функций
+	py::module_ kopis_extra = m.def_submodule("kopis_extra", "Submodule for additional functionality");
+
+	kopis_extra.def("test_gpio_pins", []() {
+		tests::RunGpioPinsTest(*g_assembly);
+		}, "runs test program for GPIO pins");
+
+	kopis_extra.def("test_motors", []() {
+		tests::RunMotorsTest(*g_assembly);
+		}, "runs test program for motors");
+
+	kopis_extra.def("print_board_pins", []() {
+		g_board->printBoardPins();
+		}, "prints table with all pins information");
 }
