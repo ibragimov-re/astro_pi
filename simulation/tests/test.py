ecac7093e94739ec4eb08662ab1d4a23385d449a
Ruslan <ibrmov.rusl@mail.ru>
2025-11-20 00:11:30 +0500

    test(simulation): improve python demonstration test file

--- simulation/tests/test.py
+++ simulation/tests/test.py
@@ -1,23 +1,60 @@
 import kopis as GPIO
 import time
 
-GPIO.setmode(GPIO.SUNXI)
 
+print("================================= Работа с пинами =================================")
+
+print("Установка режима нумерации пинов")
+GPIO.setmode(GPIO.BOARD)
+
+print("Установка режимов у пинов")
 GPIO.setup(8, GPIO.IN)
 GPIO.setup(12,GPIO.OUT)
+time.sleep(2)
 
-
+print("Изменение состояния пина")
 GPIO.output(12, GPIO.HIGH)
+time.sleep(2)
 
-for i in range(0,10):
+print("Цикличное изменение состояния пина")
+for i in range(0, 10):
     GPIO.setup(8, GPIO.IN)
     time.sleep(0.1)
     GPIO.setup(8, GPIO.OUT)
     time.sleep(0.1)
+time.sleep(2)
 
-
+print("Очистка пина")
 GPIO.cleanup(12)
-time.sleep(5)
-print ("123")
+time.sleep(2)
+
+print("Очистка всех пинов")
 GPIO.cleanup()
-time.sleep(10)
+time.sleep(2)
+
+
+print("================================ Работа с моторами ================================")
+
+horizontal_motor_name = "#Nema17HS8401_Horizontal"
+vertical_motor_name = "#Nema17HS8401_Vertical"
+
+print("Перемещение моторов")
+GPIO.kopis_motorsim.move_degrees(horizontal_motor_name, 230, 3)
+GPIO.kopis_motorsim.move_degrees(vertical_motor_name, -40, 1)
+time.sleep(2)
+
+print("Выставление моторов в исходное положение")
+GPIO.kopis_motorsim.reset_angle(horizontal_motor_name)
+GPIO.kopis_motorsim.reset_angle(vertical_motor_name)
+time.sleep(2)
+
+
+print("====================== Использование дополнительных функций ======================")
+
+print("Печать таблицы со всей информацией о пинах")
+GPIO.kopis_extra.print_board_pins()
+time.sleep(2)
+
+print("Запуск заготовленных тестовых управляющих программ, реализованных внутри библиотеки симулятора (в C++ коде)")
+GPIO.kopis_extra.test_motors()
+GPIO.kopis_extra.test_gpio_pins()
